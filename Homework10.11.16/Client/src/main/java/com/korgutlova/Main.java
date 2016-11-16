838fc2a515e10532e9a6369bddcb5075287be488
Korgutlova <korgutlova97@yandex.ru>
2016-11-16 22:07:19 +0300

    Revert "[AY-1] : Maven corrected."

--- Homework10.11.16/Client/src/main/java/com/korgutlova/Main.java
+++
@@ -1,8 +0,0 @@
-package com.korgutlova;
-
-public class Main {
-    public static void main(String[] args) {
-        Client client = new Client("Masha", 10000);
-        System.out.println(client.buySomething(3000, "GUM"));
-    }
-}
